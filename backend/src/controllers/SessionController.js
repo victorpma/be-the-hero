73c06b99cd9a16932bc4d06ec922e8f320f8fd83
Victor <vpma_amorim@hotmail.com>
2020-03-26 21:01:02 -0300

    Creating SessionController

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,17 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async signIn(request, response) {
+        const { id } = request.body;
+
+        const ong = await connection('ongs')
+            .where('id', id)
+            .select('name')
+            .first();
+
+        if (!ong)
+            return response.status(401).json({ error: 'Não foi encontrada ONG com este ID!' });
+
+        return response.json({ ong });
+    }
+}

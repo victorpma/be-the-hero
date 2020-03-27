73204cb520886185cdf1ca273679795b3394a746
Victor <vpma_amorim@hotmail.com>
2020-03-26 21:01:16 -0300

    Install package Cors

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,19 @@ const express = require('express');
 const routes = express.Router();
 
 const ongController = require('./controllers/OngController');
+const incidentController = require('./controllers/IncidentController');
+const profileController = require('./controllers/ProfileController');
+const sessionController = require('./controllers/SessionController');
 
 routes.get('/ongs', ongController.list);
 routes.post('/ongs', ongController.create);
+
+routes.get('/incidents', incidentController.list);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete);
+
+routes.get('/profile', profileController.index);
+
+routes.post('/session', sessionController.signIn);
 
 module.exports = routes;

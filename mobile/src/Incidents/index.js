13152263c6bec0ee0a78f18a47e5142f65562d54
Victor <vpma_amorim@hotmail.com>
2020-03-29 22:44:48 -0300

    Integration of page Incidents with api

--- mobile/src/Incidents/index.js
+++ mobile/src/Incidents/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
+
+import api from '../services/api';
 
 import logoImg from '../assets/logo.png';
 
@@ -9,10 +11,35 @@ import styles from './styles';
 
 function Incidents() {
     const navigation = useNavigation();
+    const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(false);
 
-    function navigateToDetail() {
-        navigation.navigate('Detail');
+    function navigateToDetail(incident) {
+        navigation.navigate('Detail', { incident });
     }
+
+    async function loadIncidents() {
+        if (loading) return;
+
+        if (total > 0 && total == incidents.length) return;
+
+        setLoading(true);
+
+        const response = await api.get('incidents', {
+            params: { page }
+        });
+
+        setIncidents([...incidents, ...response.data]);
+        setTotal(response.headers['x-total-count']);
+        setPage(page + 1);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        loadIncidents();
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -20,7 +47,7 @@ function Incidents() {
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
                     Total de
-                    <Text style={styles.headerTextBold}> 0 casos</Text>
+                    <Text style={styles.headerTextBold}> {total} casos</Text>
                 </Text>
             </View>
 
@@ -28,22 +55,30 @@ function Incidents() {
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia</Text>
 
             <FlatList
-                data={[1, 2, 3]}
+                data={incidents}
                 style={styles.incidentsList}
-                keyExtractor={incident => String(incident)}
+                keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
+                renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>Caso</Text>
-                        <Text style={styles.incidentValue}>Cadelinhas atropelada</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>ONG</Text>
-                        <Text style={styles.incidentValue}>AVOSOS</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>Valor</Text>
-                        <Text style={styles.incidentValue}>R$ 120,00 reais</Text>
+                        <Text style={styles.incidentValue}>{
+                            Intl.NumberFormat('pt-BR',
+                                {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(incident.value)}
+                        </Text>
 
-                        <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
+                        <TouchableOpacity style={styles.detailsButton} onPress={() => navigateToDetail(incident)}>
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                             <Feather style={styles.detailsButtonIcon} name="arrow-right" size={16} color="#E02041" />
                         </TouchableOpacity>

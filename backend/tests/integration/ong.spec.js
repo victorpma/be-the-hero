52e08b783db0c5789dc2c2ffcfceff445955cc42
Victor <vpma_amorim@hotmail.com>
2020-03-31 20:03:49 -0300

    Implementing integration tests

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -4,7 +4,12 @@ const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
     beforeEach(async () => {
-        await connection.migrate();
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll(() => {
+        connection.detroy();
     });
 
     it('should be able to create a new ONG', async () => {
@@ -18,6 +23,7 @@ describe('ONG', () => {
                 uf: "SE"
             });
 
-        console.log(response.body);
+        expect(response.body).toHaveProperty("id");
+        expect(response.body.id).toHaveLength(8);
     });
 });
